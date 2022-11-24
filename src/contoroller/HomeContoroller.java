1f46e6b5bc8db6d0db98d79e1f4b8c5211f404c8
nmt667r <nm66flat@gmial.com>
2022-11-25 07:12:48 +0900

    【タイトル】スクレイピング処理細分化・メソッド分け 【変更内容】スクレインピング処理をクラス・タグで細分化し、配列として処理した 【変更理由】-

--- src/contoroller/HomeContoroller.java
+++ src/contoroller/HomeContoroller.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import beans.Player;
@@ -13,22 +12,46 @@ public class HomeContoroller {
 
 
     public static void main(String[] args) throws IOException {
-//    	getStats(args);
-    	List<Player> players = new PlayerService().select();
+    	getStats(args);
+    }
 
+    public static void getStats(String[] args) throws IOException {
+    	//全htmlデータ取得
+    	Document document = Jsoup.connect("https://baseball.yahoo.co.jp/npb/stats/batter?gameKindId=1&type=avg").get();
+    	//クラス・タグで絞って配列化
+    	Elements statsArray = document.getElementsByClass("bb-playerTable__row").select("tr");
+   	
+    	//プレイヤー配列を用意
+    	//List<Player> players = new ArrayList<Player>();
+    	
+    	//スクレイピングデータをループして出力
+    	for (int i = 1; i < statsArray.size(); i++) {
+    		if(i != statsArray.size()-1) {
+    			String[] data = statsArray.get(i).text().split(" ");
+        		for (int y = 0; y < data.length; y++) {
+        			System.out.println(data[y]);
+        		}	
+    		}
+    	}
+    }
+    
+	public static void getStatsAll(String[] args) throws IOException {
+	   	Document document = Jsoup.connect("https://baseball.yahoo.co.jp/npb/stats/batter?gameKindId=1&type=avg").get();
+    	//クラス・タグで絞って配列化
+        Elements statsArray = document.getElementsByClass("bb-playerTable__row");
+        System.out.println(statsArray);
+//        for (Element str: statsArray){
+//        		System.out.println(str.getElementsByClass("bb-playerTable__data"));
+//        }
+	}
+	
+	//DB接続用メソッド
+	public static void getDBstats(String[] args) throws IOException {
+		List<Player> players = new PlayerService().select();
     	for (Player p: players){
     		System.out.println(p.getId());
     		System.out.println(p.getName());
     	}
-    }
-
-    public static void getStats(String[] args) throws IOException {
-    	Document document = Jsoup.connect("https://baseball.yahoo.co.jp/npb/stats/batter?gameKindId=1&type=avg").get();
-
-        Elements statsArray = document.getElementsByClass("bb-playerTable__row");
-        for (Element str: statsArray){
-        		System.out.println(str.getElementsByClass("bb-playerTable__data").text());
-        }
-    }
+	}
 
 }
