b6020fe68143e7f294e5bc2165ae0f0ead027f29
nmt667r <nm66flat@gmial.com>
2022-12-05 06:09:53 +0900

    【タイトル】スタッツの入力・出力処理をクラス化 【変更内容】スタッツの入力・出力処理をクラスし、mainから呼び出す 【変更理由】mainクラスの煩雑化を防ぐため

--- src/contoroller/HomeContoroller.java
+++ src/contoroller/HomeContoroller.java
@@ -1,10 +1,10 @@
 package contoroller;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import beans.Player;
@@ -13,47 +13,22 @@ public class HomeContoroller {
 
 
     public static void main(String[] args) throws IOException {
-    	getStats(args);
+//    	getStatsAll(args);
+    	List<Player> players = GetPlayerStatsWebAPI.getStats(args);
+    	GetPlayerStatsWebAPI.printStats(players);
     }
 
-    public static void getStats(String[] args) throws IOException {
-    	//全htmlデータ取得
-    	Document document = Jsoup.connect("https://baseball.yahoo.co.jp/npb/stats/batter?gameKindId=1&type=avg").get();
-    	//クラス・タグで絞って配列化
-    	Elements statsArray = document.getElementsByClass("bb-playerTable__row").select("tr");
-   	
-    	//プレイヤー配列を用意
-    	List<Player> players = new ArrayList<Player>();
-    	
-    	//スクレイピングデータをループして出力
-    	for (int i = 1; i < statsArray.size(); i++) {
-    		Player player = new Player();
-    		if(i != statsArray.size()-1) {
-    			String[] data = statsArray.get(i).text().split(" ");
 
-    			player.setId(i);
-        		player.setName(data[1]);
-        		player.setBattingAverage(Double.parseDouble(data[3]));
-        		players.add(player);
-        		
-    		}
-    		
-    		System.out.println(player.getId());
-    		System.out.println(player.getName());
-    		System.out.println(player.getBattingAverage());
-    	}
-        
-
-    }
     
 	public static void getStatsAll(String[] args) throws IOException {
 	   	Document document = Jsoup.connect("https://baseball.yahoo.co.jp/npb/stats/batter?gameKindId=1&type=avg").get();
     	//クラス・タグで絞って配列化
         Elements statsArray = document.getElementsByClass("bb-playerTable__row");
         System.out.println(statsArray);
-//        for (Element str: statsArray){
-//        		System.out.println(str.getElementsByClass("bb-playerTable__data"));
-//        }
+        
+        for (Element str: statsArray){
+        		System.out.println(str.getElementsByClass("bb-playerTable__data"));
+        }
 	}
 	
 	//DB接続用メソッド
