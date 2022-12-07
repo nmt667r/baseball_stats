054c5aded6886028f2326c72aaa65b5326ae3948
nmt667r <nm66flat@gmial.com>
2022-12-08 07:01:01 +0900

    【タイトル】test用クラス作成 【変更内容】mainからtest用クラスを分離 【変更理由】-

--- src/contoroller/HomeContoroller.java
+++ src/contoroller/HomeContoroller.java
@@ -2,43 +2,15 @@ package contoroller;
 import java.io.IOException;
 import java.util.List;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import beans.Player;
 import service.PlayerService;
 public class HomeContoroller {
 
 
     public static void main(String[] args) throws IOException {
-//    	getStatsAll(args);
+//    	test.getStatsAll(args);
     	List<Player> players = GetPlayerStatsWebAPI.getStats(args);
 //    	GetPlayerStatsWebAPI.printStats(players);
     	new PlayerService().insert(players);
     }
-
-
-    
-	public static void getStatsAll(String[] args) throws IOException {
-	   	Document document = Jsoup.connect("https://baseball.yahoo.co.jp/npb/stats/batter?gameKindId=1&type=avg").get();
-    	//クラス・タグで絞って配列化
-        Elements statsArray = document.getElementsByClass("bb-playerTable__row");
-        System.out.println(statsArray);
-        
-        for (Element str: statsArray){
-        		System.out.println(str.getElementsByClass("bb-playerTable__data"));
-        }
-	}
-	
-	//DB接続用メソッド
-	public static void getDBstats(String[] args) throws IOException {
-		List<Player> players = new PlayerService().select();
-    	for (Player p: players){
-    		System.out.println(p.getId());
-    		System.out.println(p.getName());
-    	}
-	}
-
 }
