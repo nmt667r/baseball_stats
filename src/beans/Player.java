c7d60244f7d7d3b3071d4f7d3c7ac6442a0bfd20
nmt667r <nm66flat@gmial.com>
2022-12-08 07:57:26 +0900

    【タイトル】選手情報のセッターゲッターの新規作成 【変更内容】選手情報のセッターゲッターの新規作成 【変更理由】-

---
+++ src/beans/Player.java
@@ -0,0 +1,32 @@
+package beans;
+
+public class Player {
+	private int id; //ID
+	private String name; //選手名
+	private String league; //選手名
+	private String team; //選手名
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLeague() {
+		return league;
+	}
+	public void setLeague(String league) {
+		this.league = league;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+}
